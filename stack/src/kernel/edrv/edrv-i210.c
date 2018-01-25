74c83e5d62e964993058a5ba3bbf9d0ed9b088d5
Your Name <you@example.com>
2018-01-25 15:51:01 +0100

    Fix compilation of I210 edrv on Linux 4.8+

--- stack/src/kernel/edrv/edrv-i210.c
+++ stack/src/kernel/edrv/edrv-i210.c
@@ -2754,7 +2754,12 @@ static int initOnePciDev(struct pci_dev* pPciDev_p, const struct pci_device_id* 
             edrvInstance_l.pMsixEntry[index].entry = index;
         }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,8,0)
         result = pci_enable_msix(pPciDev_p, edrvInstance_l.pMsixEntry, numVectors);
+#else
+        result = pci_enable_msix_range(pPciDev_p, edrvInstance_l.pMsixEntry,
+                                       numVectors, numVectors);
+#endif
         if (result != 0)
         {
             printk("...Failed\n");
